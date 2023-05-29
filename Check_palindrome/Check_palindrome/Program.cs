116b6e6d79f51d607345967c5f97096d9047c107
Peter.Chiu <tidub76217@hotmail.com>
2023-05-29 15:44:17 +0800

    迴文測試
    Check_palindrome

---
+++ Check_palindrome/Check_palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_palindrome
+{
+    class Program
+    {
+        /// <summary>
+        /// 回文判斷
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int s = 123321;
+
+            Console.WriteLine(IsPalindrome(s));
+            Console.ReadKey();
+
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            string s_input = x.ToString();
+            string res = "";
+
+            for (int i = s_input.Length - 1; i >= 0; i--)
+            {
+                res += s_input[i];
+            }
+
+            if (res == s_input)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+    }
+}
