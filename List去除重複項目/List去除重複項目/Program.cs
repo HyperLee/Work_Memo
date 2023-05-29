fee4c7f48ed5ea194d86c8ec4886555473414b9f
Peter.Chiu <tidub76217@hotmail.com>
2023-05-29 11:02:57 +0800

    memo
    1.list去除重複項目
    2.二分法查找

---
+++ List去除重複項目/List去除重複項目/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List去除重複項目
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 工作上 專案  新增公告 碰到之 問題
+        /// 解法 紀錄
+        /// 
+        /// input1: 新增名單
+        /// input2: 刪除名單
+        /// => 新增 要扣除 與 刪除 重複 之項目
+        ///    以及 只有 刪除 才出現之項目 也要扣除
+        ///    
+        /// 最終得到的 才是所需要的
+        /// 
+        /// 參考: 
+        /// https://www.itread01.com/article/1481080283.html
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            List<string> Ashuzu = new List<string>();
+            Ashuzu.Add("1");
+            Ashuzu.Add("2");
+            Ashuzu.Add("3");
+            Ashuzu.Add("5");
+            List<string> Bshuzu = new List<string>();
+            Bshuzu.Add("1");
+            Bshuzu.Add("2");
+            Bshuzu.Add("4");
+            List<string> Cshuzu = new List<string>();
+            foreach (string aitem in Ashuzu)
+            {
+                if (!Bshuzu.Contains(aitem))
+                {
+                    Cshuzu.Add(aitem);
+                }
+            }
+
+            foreach( var value in Cshuzu)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 二分法找mid寫法/二分法找mid寫法/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 二分法找mid寫法
+{
+    internal class Program
+    {
+        /// <summary>
+        /// https://blog.csdn.net/m0_50086696/article/details/123353057
+        /// 二分法找mid 
+        /// 兩種寫法 
+        /// 測試
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int left = 0;
+            int right = 11;
+
+            Method1(left, right);
+            Method2(left, right);
+
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// mid = (left + right) / 2;
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public static void Method1(int left, int right)
+        {
+            int mid = 0;
+            mid = (left + right) / 2;
+
+            Console.WriteLine("Method1: " + mid);
+
+        }
+
+
+        /// <summary>
+        ///  mid = (right - left) / 2 + left;
+        ///  此方法通常是用來防止 溢位 使用
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        public static void Method2(int left, int right)
+        {
+            int mid = 0;
+            mid = (right - left) / 2 + left;
+
+            Console.WriteLine("Method2: " + mid);
+
+        }
+    }
+}
