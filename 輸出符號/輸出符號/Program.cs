a658797f0d68bb80d64a59b3541357436c0cb929
Peter.Chiu <tidub76217@hotmail.com>
2023-07-03 16:46:45 +0800

    輸出雙引號
    輸出雙引號

---
+++ 輸出符號/輸出符號/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 輸出符號
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 輸出雙引號; 要加入 \ 才可以
+            Console.WriteLine("\"\"");
+            Console.WriteLine("'");
+            Console.WriteLine("\"");
+            Console.WriteLine("''");
+
+
+            Console.ReadKey();
+        }
+    }
+}
