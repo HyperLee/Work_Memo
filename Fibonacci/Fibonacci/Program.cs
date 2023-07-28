59a3b3bc68f6c3dc81fe87082b3e7699aac395e6
Peter.Chiu <tidub76217@hotmail.com>
2023-07-28 09:51:47 +0800

    fibonacci
    fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        /// <summary>
+        /// fibonacci
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Console.WriteLine(Fibo(n));
+            Console.ReadKey();
+        }
+
+        public static int Fibo(int n)
+        {
+            if(n <= 1)
+            {
+                return n;
+            }
+
+            int first = 0;
+            int second = 1;
+            int result = 0;
+
+            for(int i = 2; i <= n; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+
+            return result;
+        }
+
+    }
+}
