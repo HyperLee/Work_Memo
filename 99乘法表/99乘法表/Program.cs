19928515f090e4258c335197f633d2ef0bee62b2
Peter.Chiu <tidub76217@hotmail.com>
2023-07-28 09:53:31 +0800

    99乘法表
    99乘法表

---
+++ 99乘法表/99乘法表/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99乘法表
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 99乘法表
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            for(int i = 1; i<= 9; i++)
+            {
+                for(int j = 1; j<= 9; j++)
+                {
+                    Console.WriteLine(i * j);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
