322bb8f3393016c8646f474d02188da400e36760
Peter.Chiu <tidub76217@hotmail.com>
2023-07-28 09:57:22 +0800

    輸入15個整數 排序 輸出
    輸入15個整數 排序 輸出

---
+++ 輸入整數排序/輸入整數排序/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 輸入整數排序
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 輸入15個整數 排序 輸出
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請輸入15個數字");
+
+            int[] num = new int[15];
+
+            for(int i = 0; i < 15; i++)
+            {
+                num[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(num);
+
+            foreach(int i in num) 
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
