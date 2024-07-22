00e200eb4727e0c68e2df170046c236897913206
Peter.Chiu <tidub76217@hotmail.com>
2024-07-22 14:06:43 +0800

    Fibonacci
    調整寫法與註解

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -14,22 +14,33 @@ namespace Fibonacci
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int n = 10;
+            int n = 100;
             Console.WriteLine(Fibo(n));
             Console.ReadKey();
         }
 
-        public static int Fibo(int n)
+
+        /// <summary>
+        /// 迴圈從2開始
+        /// result 是取前兩數相加之和
+        /// if 那邊就可以得知 n = 0, 1之結果
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static long Fibo(int n)
         {
             if(n <= 1)
             {
                 return n;
             }
 
-            int first = 0;
-            int second = 1;
-            int result = 0;
+            // n = 0, result = 0
+            long first = 0;
+            // n = 1, result = 1
+            long second = 1;
+            long result = 0;
 
+            // 要注意這邊是從2開始, 
             for(int i = 2; i <= n; i++)
             {
                 result = first + second;
