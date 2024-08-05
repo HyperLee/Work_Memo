1c0471449081df22778c153f7ad674dc35c83f29
Peter.Chiu <tidub76217@hotmail.com>
2024-08-05 15:07:03 +0800

    日期範圍測試
    測試日期 增減

---
+++ 日期範圍測試/日期範圍測試/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace 日期範圍測試
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime LDs, LDe;
+            int cycle7day = 7;
+            try
+            {
+                LDe = DateTime.Today;
+
+                // - 7 前七天時間
+                LDs = LDe.AddDays(Convert.ToInt32('-' + cycle7day));
+
+                Console.WriteLine("LDs: " + LDs);
+
+                Console.WriteLine("LDe: " + LDe);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("test: " + Convert.ToInt32(cycle7day));
+
+            Console.ReadKey();
+        }
+    }
+}
