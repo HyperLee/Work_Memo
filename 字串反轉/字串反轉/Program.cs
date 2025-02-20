1a052eb1793c563b8adc6f6c6a4ed5635db3fabb
Peter.Chiu <tidub76217@hotmail.com>
2025-02-20 10:59:16 +0800

    字串反轉
    字串反轉

--- 字串反轉/字串反轉/Program.cs
+++ 字串反轉/字串反轉/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace 字串反轉
 {
@@ -11,11 +7,10 @@ namespace 字串反轉
         static void Main(string[] args)
         {
             string str = "123ABC";
-            
+            Console.WriteLine("原始字串: " + str);
             Console.WriteLine("方法1: " + Reverse(str));
             Console.WriteLine("方法2: " + Reverse2(str));
-
-            Console.ReadKey();
+            Console.WriteLine("方法3: " + Reverse3(str));
 
         }
 
@@ -23,6 +18,9 @@ namespace 字串反轉
         /// <summary>
         /// 字串反轉
         /// 字符串轉換為字符數組 String.ToCharArray()
+        /// 
+        /// ReverseString方法將字串轉換為字符陣列，
+        /// 然後使用Array.Reverse方法反轉字符陣列，最後將其轉換回字串。
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
@@ -37,6 +35,8 @@ namespace 字串反轉
         /// <summary>
         /// 不使用內建 Reverse反轉
         /// 自己寫反轉
+        /// 
+        /// 使用StringBuilder： StringBuilder在處理大量字串操作時更高效。
         /// </summary>
         /// <param name="str"></param>
         /// <returns></returns>
@@ -44,7 +44,7 @@ namespace 字串反轉
         public static string Reverse2(string str)
         {
             var sb = new StringBuilder();
-            for(var i = str.Length - 1; i >= 0; i--)
+            for (var i = str.Length - 1; i >= 0; i--)
             {
                 sb.Append(str[i]);
             }
@@ -52,5 +52,20 @@ namespace 字串反轉
             return sb.ToString();
         }
 
+
+        /// <summary>
+        /// 使用迴圈反轉
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string Reverse3(string str)
+        {
+            string reversed = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            return reversed;
+        }
     }
 }
