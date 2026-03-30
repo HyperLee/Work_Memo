dc19c82acceddfad114ad60c77c91ec96bc13711
Peter.Chiu <tidub76217@hotmail.com>
2026-03-30 14:44:06 +0800

    feat: 新增 WordPattern 方法及 VSCode 配置檔

--- DictionaryEquals用法/DictionaryEquals用法/Program.cs
+++ DictionaryEquals用法/DictionaryEquals用法/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+
+        /// <summary>
+        /// Dictionary.Equals 用法
+        /// 比對 value 是否相同
+        /// 感覺很實用
+        /// 需要特別紀錄
+        /// </summary>
+        /// <param name="pattern"></param>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> dic1 = new Dictionary<char, string>();
+            Dictionary<string, char> dic2 = new Dictionary<string, char>();
+            int length = pattern.Length;
+            string[] arr = s.Split(new char[] { ' ' });
+
+            if (arr.Length != length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                char c = pattern[i];
+                string word = arr[i];
+
+                if (!dic1.ContainsKey(c))
+                {
+                    dic1.Add(c, word);
+                }
+                else if (!dic1[c].Equals(word))
+                {
+                    return false;
+                }
+
+                if (!dic2.ContainsKey(word))
+                {
+                    dic2.Add(word, c);
+                }
+                else if (!dic2[word].Equals(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
